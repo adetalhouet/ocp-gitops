155d91ad83a6c470dfc90d122a6959c64097b5f1
Alexis de Talhouët <adetalhouet89@gmail.com>
2021-07-09 11:14:00 -0400

    Revamp my gitops strategy
    Signed-off-by: Alexis de Talhouët <adetalhouet89@gmail.com>

--- base/config/infra-node/infra-node-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Ingress setup
-oc patch ingresscontroller/default -n  openshift-ingress-operator  --type=merge -p '{"spec":{"nodePlacement": {"nodeSelector": {"matchLabels": {"node-role.kubernetes.io/infra": ""}}}}}'
-oc patch ingresscontroller/default -n openshift-ingress-operator --type=merge -p '{"spec":{"replicas": 3}}'
-
-# Registry setup
-oc patch configs.imageregistry.operator.openshift.io/cluster --type=merge -p '{"spec":{"nodeSelector": {"node-role.kubernetes.io/infra": ""}}}'
---
+++ bootstrap/README.md
@@ -0,0 +1,6 @@
+# Bootstrap
+
+The boostrap is responsible to create:
+- sealed-secret namespace
+- (optional) sealed-secret secret, with your keypair
+- cluster-config-manager Argo CD application acting as app-of-apps
