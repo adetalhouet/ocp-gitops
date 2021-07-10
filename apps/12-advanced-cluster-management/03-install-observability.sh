d7ddfb4bc7afe7fb1fa8860aeba9d03faf525319
Alexis de Talhouët <adetalhouet89@gmail.com>
2021-07-10 18:30:08 -0400

    Update ACM config
    Signed-off-by: Alexis de Talhouët <adetalhouet89@gmail.com>

--- apps/12-advanced-cluster-management/03-install-observability.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DOCKER_CONFIG_JSON=`oc extract secret/pull-secret -n openshift-config --to=-`
-
-oc create secret generic multiclusterhub-operator-pull-secret \
-    -n open-cluster-management-observability \
-    --from-literal=.dockerconfigjson="$DOCKER_CONFIG_JSON" \
-    --type=kubernetes.io/dockerconfigjson
