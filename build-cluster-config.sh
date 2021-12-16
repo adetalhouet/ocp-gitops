03a2f68b0b40c6372435da17ec8f3fcfd9448215
Alexis de Talhouët <adetalhouet89@gmail.com>
2021-12-16 12:47:49 -0500

    Adjust build cluster config script
    Signed-off-by: Alexis de Talhouët <adetalhouet89@gmail.com>

--- build-cluster-config.sh
+++ build-cluster-config.sh
@@ -18,11 +18,10 @@ fi
 # prepare schafolding
 cp -r bootstrap/default bootstrap/$CLUSTER_NAME
 cp -r clusters/default clusters/$CLUSTER_NAME
-cp -r apps/01-openshift-gitops/overlay/default apps/01-openshift-gitops/overlay/$CLUSTER_NAME
-cp -r apps/06-rhsso/overlay/default apps/06-rhsso/overlay/$CLUSTER_NAME
-cp -r apps/07-oauth/overlay/default apps/07-oauth/overlay/$CLUSTER_NAME
-cp -r apps/10-ansible-automation-platform/overlay/default apps/10-ansible-automation-platform/overlay/$CLUSTER_NAME
-cp -r apps/13-acs/overlay/default apps/13-acs/overlay/$CLUSTER_NAME
+cp -r apps/01-openshift-gitops/overlays/default apps/01-openshift-gitops/overlays/$CLUSTER_NAME
+cp -r apps/06-rhsso/overlays/default apps/06-rhsso/overlays/$CLUSTER_NAME
+cp -r apps/07-oauth/overlays/default apps/07-oauth/overlays/$CLUSTER_NAME
+cp -r apps/16-acs/overlays/default apps/16-acs/overlays/$CLUSTER_NAME
 
 # replace fqdn cluster name
 find . -type f -path "*$CLUSTER_NAME*" -exec gsed -i "s/hub-adetalhouet.rhtelco.io/$CLUSTER_NAME.$DOMAIN_NAME/g" {} +
@@ -32,9 +31,9 @@ gsed -i "s/clusters\/default/clusters\/$CLUSTER_NAME/g" bootstrap/$CLUSTER_NAME/
 
 # configure cluster
 gsed -i "s/cluster: default/cluster: $CLUSTER_NAME/g" clusters/$CLUSTER_NAME/applicationset.yaml
-gsed -i "s/overlay\/default/overlay\/$CLUSTER_NAME/g" clusters/$CLUSTER_NAME/applicationset.yaml
+gsed -i "s/overlays\/default/overlays\/$CLUSTER_NAME/g" clusters/$CLUSTER_NAME/applicationset.yaml
 
 # Regenerate RH SSO Sealed Secret
-RH_SSO_OVERLAY=apps/06-rhsso/overlay/$CLUSTER_NAME
+RH_SSO_OVERLAY=apps/06-rhsso/overlays/$CLUSTER_NAME
 kustomize build $RH_SSO_OVERLAY/config > $RH_SSO_OVERLAY/config/rhsso-config.yaml
 kubeseal --cert ~/.bitnami/tls.crt --format yaml < $RH_SSO_OVERLAY/config/rhsso-config.yaml > $RH_SSO_OVERLAY/01-sealed-rhsso-config.yaml
